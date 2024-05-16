d7ab1b53c62afddadfee87a24485c5df2ca8dfde
chirongaA <a_chironga@students.uonbi.ac.ke>
2024-05-16 14:08:59 +0300

    test

--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 include_once 'routes.php';
 //Get the uri being requested
 $uri = $_SERVER['REQUEST_URI'];
-//Set the header to json
-header('Content-Type: application/json');
 //Create a new router object
 $router->route($uri);
 
