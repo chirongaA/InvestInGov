d9d83894e98e32318f15b43f62b86a5a479d6c86
chirongaA <a_chironga@students.uonbi.ac.ke>
2024-05-18 11:25:20 +0300

    bids form creation on php and action on bids.html

--- models/bids.models.php
+++ models/bids.models.php
@@ -2,16 +2,17 @@
 //Include the database class
 require_once './models/database.class.php';
 
-//Create the User class as a child class of the database class
-class User extends Database{
+//Create the Bids class as a child class of the database class
+class Bids extends Database{
     //The table name
     const TABLE_NAME = 'bids';
     //Members for the user class
     private $username=null;
     private $bond_id=null;
     private $yield=null;
-    private $phone_number=null;
-    
+    private $facevalue=null;
+    private $maturity=null;
+    private $rate=null;
     
     //The constructor
     function __construct()
@@ -19,24 +20,27 @@ class User extends Database{
         //Create a connection
         parent::__construct();
     }
-    //Method to add a new user to the database
-    public function create($user):bool
+    //Method to add a new bid to the database
+    public function create($bid):bool
     {
         //Escape the user data
-        $username = $this->escape($user['username']);
-        $password = $this->escape($user['bond_id']);
-        $email = $this->escape($user['yield']);
-        $phone_number = $this->escape($user['phone_number']);
+        $username = $this->escape($bid['username']);
+        $bond_id = $this->escape($bid['bond_id']);
+        $yield = $this->escape($bid['yield']);
+        $facevalue = $this->escape($bid['facevalue']);
+        $maturity = $this->escape($bid['maturity']);
+        $rate = $this->escape($bid['rate']);
         //Create the query
-        $query = "INSERT INTO $table_name (username, bond_id, yield, phone_number) VALUES ('$username', '$bond', '$email','$id_number', '$kra_pin_number', '$phone_number', '$full_name')";
+        $table_name = self::TABLE_NAME;
+        $query = "INSERT INTO $table_name (username, bond_id, yield, facevalue, maturity, rate) VALUES ('$username', '$bond_id', '$yield','$facevalue', '$maturity', '$rate')";
         //Execute the query
         $exec=$this->query($query);
         if($exec)
         {
             //Return the last inserted id
-            $this->user_id=$this->lastInsertId();
+            $this->bond_id=$this->lastInsertId();
             //Set Data
-            $this->setData($user);
+            $this->setData($bid);
             return true;
         }
         else
@@ -44,14 +48,14 @@ class User extends Database{
             return false;
         }
     }
-    //To set the data of the user
+    //To set the data of the bid
     private function setData($data):bool {
         $this->username = $data['username'];
-        $this->password = $data['password'];
-        $this->email = $data['email'];
-        $this->kra_pin_number = $data['kra_pin_number'];
-        $this->phone_number = $data['phone_number'];
-        $this->full_name = $data['full_name'];
+        $this->bond_id = $data['bond_id'];
+        $this->yield = $data['yield'];
+        $this->facevalue = $data['facevalue'];
+        $this->maturity = $data['maturity'];
+        $this->rate = $data['rate'];
         return true;
     }
 }
