d674781f6ea845ea929a5e43aa720af947732714
chirongaA <a_chironga@students.uonbi.ac.ke>
2024-05-28 18:51:48 +0300

    stkresp

--- tests/accessToken.php
+++ tests/accessToken.php
@@ -10,10 +10,10 @@ $curl= curl_init($access_token_url);
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($curl, CURLOPT_HEADER, FALSE);
-curl_setopt($curl, CURLOPT_USERPWD, $consumerKey.':'.$consumerSecret);
-$result=curl_exec($curl);
-$status=curl_getinfo($curl, CURLINFO_HTTP_CODE);
+curl_setopt($curl, CURLOPT_USERPWD, $consumerKey . ':' . $consumerSecret);
+$result= curl_exec($curl);
+$status= curl_getinfo($curl, CURLINFO_HTTP_CODE);
  
-$result=json_decode($result);
-echo $access_token= $result->access_token;
+$result = json_decode($result);
+echo $access_token = $result->access_token;
 curl_close($curl);
